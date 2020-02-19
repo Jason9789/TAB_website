d9270df9b9a3f56fdf942f4158f0cb84a182c1f0
PanGeun <lifebook0809@gmail.com>
2020-02-19 20:09:36 +0900

    add calendar css

--- tab-frontend/src/pages/CalendarPage.js
+++ tab-frontend/src/pages/CalendarPage.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-// import Calendar from '../components/calendar/Calendar';
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
+import '../components/calendar/Calendar.scss';
+import styled from 'styled-components';
+import Calendar from 'react-calendar/dist/entry.nostyle';
 import Footer from '../components/common/Footer';
 
-// const MainPage = () => {
-//   return (
-//     <>
-//       <HeaderContainer />
-//       <div>Calendar Page TEST</div>
-//       <Calendar />
-//       <Footer />
-//     </>
-//   );
-// };
-
+const CalendarBlock = styled.div`
+  margin-top: 4rem;
+`;
 class MainPage extends Component {
-  state = {
-    date: new Date(),
-  };
+  //   state = {
+  //     date: new Date(),
+  //   };
 
-  onChange = date => this.setState({ date });
+  //   onChange = date => this.setState({ date });
 
   render() {
     return (
       <>
         <HeaderContainer />
         {/* <div>Calendar Page TEST</div> */}
-        <Calendar />
+        <CalendarBlock>
+          <Calendar className="react-calendar" />
+        </CalendarBlock>
         <Footer />
       </>
     );
