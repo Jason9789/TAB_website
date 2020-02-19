715f4bfa0bacd796e570e4412ec8fc6c84f75c62
PanGeun <lifebook0809@gmail.com>
2020-02-19 19:03:09 +0900

    edit change calendar

--- tab-frontend/src/pages/CalendarPage.js
+++ tab-frontend/src/pages/CalendarPage.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-import Calendar from '../components/calendar/Calendar';
+// import Calendar from '../components/calendar/Calendar';
+import Calendar from 'react-calendar';
 import Footer from '../components/common/Footer';
 
-const MainPage = () => {
-  return (
-    <>
-      <HeaderContainer />
-      <div>Calendar Page TEST</div>
-      <Calendar />
-      <Footer />
-    </>
-  );
-};
+// const MainPage = () => {
+//   return (
+//     <>
+//       <HeaderContainer />
+//       <div>Calendar Page TEST</div>
+//       <Calendar />
+//       <Footer />
+//     </>
+//   );
+// };
+
+class MainPage extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  onChange = date => this.setState({ date });
+
+  render() {
+    return (
+      <>
+        <HeaderContainer />
+        {/* <div>Calendar Page TEST</div> */}
+        <Calendar />
+        <Footer />
+      </>
+    );
+  }
+}
 
 export default MainPage;
