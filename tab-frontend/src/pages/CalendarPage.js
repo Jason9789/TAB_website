4f904b167b9cc3e8db30a6dead2650cf8fb24dea
PanGeun <lifebook0809@gmail.com>
2020-02-22 18:58:51 +0900

    edit Calendar

--- tab-frontend/src/pages/CalendarPage.js
+++ tab-frontend/src/pages/CalendarPage.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-// import Calendar from '../components/calendar/Calendar';
 import Calendar from 'react-calendar';
 import '../components/calendar/Calendar.scss';
 import styled from 'styled-components';
-// import Calendar from 'react-calendar/dist/entry.nostyle';
-// import 'react-calendar/dist/Calendar.css';
 import Footer from '../components/common/Footer';
 
 const CalendarBlock = styled.div`
   padding-top: 4rem;
 `;
 class MainPage extends Component {
-  //   state = {
-  //     date: new Date(),
-  //   };
-
-  //   onChange = date => this.setState({ date });
-
   render() {
     return (
       <>
@@ -25,7 +16,6 @@ class MainPage extends Component {
         {/* <div>Calendar Page TEST</div> */}
         <CalendarBlock>
           <Calendar className="react-calendar" />
-          {/* <Calendar /> */}
         </CalendarBlock>
         <Footer />
       </>
