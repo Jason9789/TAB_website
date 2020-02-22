c77da19a88c55e328ff1cbc011059ed7fe29940f
PanGeun <lifebook0809@gmail.com>
2020-02-22 19:36:48 +0900

    add postsQna controller

--- tab-backend/src/api/postsQna/index.js
+++ tab-backend/src/api/postsQna/index.js
@@ -1,19 +1,13 @@
 const Router = require('koa-router');
+const postsQnaCtrl = require('./postsQna.ctrl');
+
 const postsQna = new Router();
 
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
-
-postsQna.get('/', printInfo);
-postsQna.post('/', printInfo);
-postsQna.get('/:id', printInfo);
-postsQna.delete('/:id', printInfo);
-postsQna.put('/:id', printInfo);
-postsQna.patch('/:id', printInfo);
+postsQna.get('/', postsQnaCtrl.list);
+postsQna.post('/', postsQnaCtrl.write);
+postsQna.get('/:id', postsQnaCtrl.read);
+postsQna.delete('/:id', postsQnaCtrl.remove);
+postsQna.put('/:id', postsQnaCtrl.replace);
+postsQna.patch('/:id', postsQnaCtrl.update);
 
 module.exports = postsQna;
